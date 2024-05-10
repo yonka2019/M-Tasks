ac0e005849af5a3e07a12e9a7cbb3597f9f93957
Yonka <yonka2003@gmail.com>
2024-05-10 20:22:21 +0300

    test(ex1): added tests

---
+++ Part2/Test/ex1Tests.py
@@ -0,0 +1,36 @@
+from Part2 import ex1
+
+
+def test1():
+    i = 543221
+
+    o = ex1.num_len(i)
+    return i, o
+
+
+def test2():
+    i = 1243456
+
+    o = ex1.num_len(i)
+    return i, o
+
+
+def test3():
+    i = 9
+
+    o = ex1.num_len(i)
+    return i, o
+
+
+def test4():
+    i = 9
+
+    o = ex1.num_len(i)
+    return i, o
+
+
+def test5():
+    i = 0
+
+    o = ex1.num_len(i)
+    return i, o
